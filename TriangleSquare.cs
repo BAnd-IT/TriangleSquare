41950e76e6256e28e7e0334e9668300102ea2402
Андрей Бабкин <ab@fivefaces.ru>
2018-01-10 04:22:55 +0300

    done

---
+++ TriangleSquare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriangleSquareLib
+{
+    public class TriangleSquare
+    {
+        public double SquareCalc(double A, double B, double C) {
+
+            if (A <= 0 || B <= 0 || C <= 0)
+            {
+                throw new ArgumentOutOfRangeException("Value must to be more than 0");
+            }
+
+            if (A + B <= C || B + C <= A || A + C <= B)
+            {
+                throw new ArgumentOutOfRangeException("Triangle inequality");
+            }
+
+            //calculating
+            double p = (A + B + C)/2;
+            return Math.Sqrt(p*(p-A)*(p-B)*(p-C));
+        }
+    }
+}
---
+++ TriangleSquareLib.Tests/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TriangleSquareLib.Tests
+{
+    [TestClass]
+    public class TriangleSquareTests
+    {
+        [TestMethod]
+        public void Calc_3_4_5_return_6()
+        {
+            double a = 3;
+            double b = 4;
+            double c = 5;
+            double expected = 6;
+
+            TriangleSquare x = new TriangleSquare();
+            double actual = x.SquareCalc(a,b,c);
+
+            Assert.AreEqual(expected, actual, "Triangle square should have been {0}", expected);
+        }
+
+        [ExpectedException(typeof(ArgumentOutOfRangeException), "Exception not thrown!")]
+        [TestMethod]
+        public void ZeroValueCheck_ExceptionThrown()
+        {
+            double A = 3;
+            double B = 4;
+            double C = 5;
+
+            TriangleSquare x = new TriangleSquare();
+            x.SquareCalc(A, B, 0);
+            TriangleSquare y = new TriangleSquare();
+            y.SquareCalc(A, 0, C);
+            TriangleSquare z = new TriangleSquare();
+            z.SquareCalc(0, B, C);
+            
+        }
+    }
+}
